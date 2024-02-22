371ad6418380ea63159c725e5778c663df588136
karanja-susan <karanjasusan080106@gmail.com>
2024-02-22 13:43:51 +0300

    innitial commit

---
+++ jinjasue/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JinjasueConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jinjasue'
---
+++ jinjasue/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from jinjasue import views
+urlpatterns = [
+    path('', views.home,name='my_home'),
+    path('about/', views.about,name='my_about'),
+    path('contact/',views.contact,name='my_contact'),
+    path('gallery/',views.gallery,name='my_gallery')
+]
